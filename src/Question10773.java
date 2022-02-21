9e22f47829f1ffb0762b267ce1da52d8b8b2ebc6
yeonju501 <loling970124@gmail.com>
2022-02-21 19:54:02 +0900

    test

---
+++ src/Question10773.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Question10773 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [] line = br.readLine().split(" ");
+
+    }
+
+}

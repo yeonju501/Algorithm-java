b92127afd771787c88977d52fd4a0d5c212ef512
yeonju501 <loling970124@gmail.com>
2022-03-01 02:30:38 +0900

    solved 2046

---
+++ src/Question2046.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Question2046 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i=0; i < a; i++) {
+            System.out.print("#");
+        }
+    }
+}

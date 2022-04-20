4c5dd48580370103991ab4e2b3fceaf8f19783d1
yeonju501 <loling970124@gmail.com>
2022-04-20 11:50:15 +0900

    restart

---
+++ src/Solution2558.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution2558 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = Integer.parseInt(sc.nextLine());
+        b = Integer.parseInt(sc.nextLine());
+        sc.close();
+        System.out.println(a + b);
+    }
+}

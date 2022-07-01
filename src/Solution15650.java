29a1c3f59c1126e275851c35c41f8260d3ae87f5
YJ Kwon <loling970124@gmail.com>
2022-07-01 17:32:40 +0900

    solved 15650

--- src/Solution15650.java
+++ src/Solution15650.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution15649 {
+public class Solution15650 {
     static int N, M;
     static int[] arr;
     static boolean[] visited;
@@ -14,23 +14,23 @@ public class Solution15649 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];
+        arr = new int[M+1];
         visited = new boolean[N+1];
 
-        dfs(0);
+        dfs(1);
     }
 
     static void dfs(int depth) {
-        if(depth == M) {
-            for(int a : arr) {
-                System.out.print(a + " ");
+        if(depth == M+1) {
+            for(int i = 1; i < M+1; i++) {
+                System.out.print(arr[i] + " ");
             }
             System.out.println();
             return;
         }
 
         for(int i = 1; i < N+1; i++) {
-            if(!visited[i]) {
+            if(!visited[i] && arr[depth-1] < i) {
                 visited[i] = true;
                 arr[depth] = i;
                 dfs(depth+1);
@@ -38,4 +38,6 @@ public class Solution15649 {
             }
         }
     }
+
+
 }

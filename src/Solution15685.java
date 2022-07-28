014dc96b078333d5895ee5f5c0a5dab4b604f579
YJ Kwon <loling970124@gmail.com>
2022-07-28 09:25:51 +0900

    solved 1138(implementation)

--- src/Solution15685.java
+++ src/Solution15685.java
@@ -6,10 +6,9 @@ import java.util.StringTokenizer;
 
 public class Solution15685 {
     static boolean[][] map;
-    static int[] dx = {0, -1, 0, 1};
-    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0 ,1};
     public static void main(String[] args) throws IOException {
-//x, y, d, g
 //3
 //3 3 0 1
 //4 2 1 3
@@ -20,35 +19,39 @@ public class Solution15685 {
         StringTokenizer st;
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
+
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
             int g = Integer.parseInt(st.nextToken());
+
             dragon(x, y, d, g);
         }
-        int ans = 0;
-        for(int i = 0; i < 100; i++) {
-            for(int j = 0; j < 100; j++) {
-                if(map[i][j] && map[i][j+1] && map[i+1][j] && map[i+1][j+1]){
-                    ans++;
+        int cnt = 0;
+        for(int i = 0; i <= 100; i++) {
+            for(int j = 0; j <= 100; j++) {
+                if(map[i][j] && map[i][j+1] && map[i+1][j] && map[i+1][j+1]) {
+                    cnt++;
                 }
             }
         }
-        System.out.println(ans);
+        System.out.println(cnt);
+
     }
 
     static void dragon(int x, int y, int d, int g) {
-        ArrayList<Integer> dir= new ArrayList<>();
+        ArrayList<Integer> dir = new ArrayList<>();
         dir.add(d);
-        for(int i = 0; i < g; i++){
+        map[y][x] = true;
+        for(int i = 0; i < g; i++) {
             for(int j = dir.size() - 1; j >= 0; j--) {
-                dir.add((dir.get(j)+1) % 4);
+                dir.add((dir.get(j) + 1) % 4);
             }
         }
-        map[y][x] = true;
+
         for(int i = 0; i < dir.size(); i++) {
-            y += dx[dir.get(i)];
-            x += dy[dir.get(i)];
+            x += dx[dir.get(i)];
+            y += dy[dir.get(i)];
             map[y][x] = true;
         }
     }
